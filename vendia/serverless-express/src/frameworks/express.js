fd6d50b29b6c3064be9a2a04c950bc72657cbf81
Charlie Cai <charlie.cai@sap.com>
2021-11-28 21:34:00 +0800

    test

--- vendia/serverless-express/src/frameworks/express.js
+++ vendia/serverless-express/src/frameworks/express.js
@@ -1,5 +1,9 @@
 async function sendExpressRequest ({ app, request, response }) {
+  await console.log(request);
+  console.log(request);
   app.handle(request, response)
+  await console.log(response);
+  console.log(response);
 }
 
 module.exports = {
