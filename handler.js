baee10451d8a4aadff020b6443d52a757cab7d13
Charlie Cai <charlie.cai@sap.com>
2021-11-28 17:00:37 +0800

    test

--- handler.js
+++ handler.js
@@ -5,28 +5,30 @@ const app = require('./server');
 
 
 
-let serverlessExpressInstance
+// let serverlessExpressInstance
 
-function asyncTask() {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve('connected to database'), 1000)
-    })
-}
+// function asyncTask() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve('connected to database'), 1000)
+//     })
+// }
 
-async function setup(event, context) {
-    const asyncValue = await asyncTask()
-    console.log(asyncValue)
-    serverlessExpressInstance = serverlessExpress({ app })
-    return serverlessExpressInstance(event, context)
-}
+// async function setup(event, context) {
+//     const asyncValue = await asyncTask()
+//     console.log(asyncValue)
+//     serverlessExpressInstance = serverlessExpress({ app })
+//     return serverlessExpressInstance(event, context)
+// }
 
-function handler(event, context) {
+// function handler(event, context) {
 
-    console.log(event);
-    console.log(context);
-    if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
+//     // console.log(event);
+//     // console.log(context);
+//     if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
 
-    return setup(event, context)
-}
+//     return setup(event, context)
+// }
 
-exports.main = handler;
+
+
+exports.main = serverlessExpress(app);
