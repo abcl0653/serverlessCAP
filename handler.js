cb4d7b06ef92ea7c680996eddf609f4674e6abdf
Charlie Cai <charlie.cai@sap.com>
2021-11-28 17:17:47 +0800

    test

--- handler.js
+++ handler.js
@@ -1,34 +1,36 @@
 require('source-map-support/register')
 const serverlessExpress = require('./vendia/serverless-express');
-const app = require('./server');
+const app = require('./gen/srv/server');
 
 
 
 
-// let serverlessExpressInstance
+let serverlessExpressInstance
 
-// function asyncTask() {
-//     return new Promise((resolve) => {
-//         setTimeout(() => resolve('connected to database'), 1000)
-//     })
-// }
+function asyncTask() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('connected to database'), 1000)
+    })
+}
 
-// async function setup(event, context) {
-//     const asyncValue = await asyncTask()
-//     console.log(asyncValue)
-//     serverlessExpressInstance = serverlessExpress({ app })
-//     return serverlessExpressInstance(event, context)
-// }
+async function setup(event, context) {
+    const asyncValue = await asyncTask()
+    console.log(asyncValue)
+    serverlessExpressInstance = serverlessExpress({ app })
+    return serverlessExpressInstance(event, context)
+}
 
-// function handler(event, context) {
+function handler(event, context) {
 
-//     // console.log(event);
-//     // console.log(context);
-//     if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
+    // console.log(event);
+    // console.log(context);
+    if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
 
-//     return setup(event, context)
-// }
+    return setup(event, context)
+}
+
+exports.main = handler;
 
 
 
-exports.main = serverlessExpress(app);
+// exports.main = serverlessExpress(app);
