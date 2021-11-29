f8055ac7055164bf8c5bf892439d61e02c5a9ef6
Charlie Cai <charlie.cai@sap.com>
2021-11-29 10:43:58 +0800

    test

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ const all = cds.resolve('*', {});
 console.log("========all==========all===========all======");
 console.log(all);
 
+
+const fs = require('fs');
+
+// directory path
+const dir = './srv';
+
+// list all files in the directory
+fs.readdir(dir, (err, files) => {
+    if (err) {
+        throw err;
+    }
+
+    // files object contains all files names
+    // log them on console
+    files.forEach(file => {
+        console.log(file);
+    });
+});
+
+
 cds.serve("all").in(app)
 
 app.listen (PORT, ()=> console.info(`server listening on http://localhost:${PORT}`))
